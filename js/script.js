86ce0c6a851289a943866816cec6f974cda75fe7
Chel iKalina <43254524+CheliKalina@users.noreply.github.com>
2020-03-05 19:21:23 +0200

    completed Milestone 6

--- js/script.js
+++ js/script.js
@@ -1,22 +1,58 @@
-function goFetch() {
 
+document.getElementById('button-addon2').addEventListener("click", goFetch);
+
+
+function showLoader(){
+    let loader = document.createElement("div")
+    loader.classList.add("spinner-border")
+
+        let spinner = document.createElement("span")
+        spinner.classList.add("sr-only");
+
+        const wrapper = document.getElementById('wrapper');
+        loader.append(spinner);
+        wrapper.append(loader);
+
+}
+function hideLoader(){
+    document.getElementById('wrapper').style.display = 'none';
+}
+
+function goFetch() {
     let userInput = document.getElementById('userInput').value;
     const url = 'http://localhost:5050/fibonacci/' + userInput;
 
-    fetch(url)
+    if (userInput > 50) {
+        let number = document.getElementById('userInput')
+        number.setAttribute('class', 'inputError');
+
+        let error = document.getElementById('serverError')
+        error.innerText = "Can’t be larger than 50";
+        error.setAttribute('class', 'error');
+        setTimeout(function () {
+            location.reload();
+        }, 3000);
+        
+    } else if (userInput == 42) {
+        document.getElementById('serverResult').innerText = "Server Error: 42 is the meaning of life";
+        document.getElementById('serverResult').setAttribute('class', 'textError42')        
+        setTimeout(function () {
+            location.reload();
+        }, 3000);
+
+    } else {
+        showLoader();
+        fetch(url)
         .then(response =>
             response.json())
-        .then(data => {
-            console.log(data.result)
-            document.getElementById('serverResult').innerText = data.result;
-        })
+            .then(data => {
+                console.log(data.result);
+                hideLoader();
+                let result = document.getElementById('serverResult');
+                result.innerText = data.result;
+            })
+            .catch(error => {
+                throw(error);
+            })
     }
-    
-        
-
-
-
-
-
-// The response is the calculated fibonacci, present it to the user.
-// Calling the server should replace your implementation of calculating fibonacci
+}
